4fbf30aaf4d988d0fc3590d462b97adde1f166ae
jackfs1919 <100045488+jackfs1919@users.noreply.github.com>
2024-05-16 11:13:25 +0300

    внесены изменения

--- xpath_selectors.py
+++ xpath_selectors.py
@@ -18,7 +18,6 @@ try:
     element1 = browser.find_element('xpath', '//input[@placeholder = "Input your email"]').send_keys('email')
     element1 = browser.find_element('xpath', '//input[@placeholder = "Input your phone:"]').send_keys('45698231')
     element1 = browser.find_element('xpath', '//input[@placeholder = "Input your address:"]').send_keys('address')
-
     button = browser.find_element('xpath', '//button[text() = "Submit"]')
     button.click()
     alert_handler()
