8d647f08e6b4fa0518a7a4742b90928e16caf416
den <jackfs1975@gmail.com>
2024-05-21 17:56:16 +0300

    вариант из предыдущей задачи

---
+++ conftest.py
@@ -0,0 +1,29 @@
+import pytest
+import os
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select, WebDriverWait
+
+@pytest.fixture(scope="function")
+def browser():
+    pref = {
+        "download.default_directory": os.getcwd()
+    }
+    options = webdriver.ChromeOptions()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument(
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/115.0.0.0 Safari/537.36")
+    options.add_argument('--disable-infobars')
+    options.add_argument("--headless")
+    options.add_argument("--incognito")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_experimental_option("prefs", pref)
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    # browser.execute_script("window.stop();")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
